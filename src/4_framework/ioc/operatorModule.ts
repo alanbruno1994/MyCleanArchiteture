a2967ec51540180835d6baa912168c86137a90dd
alanbruno1994 <bruno47rios@outlook.com>
2022-01-18 16:24:43 -0300

    feat: add new elements of inject dependency

--- src/4_framework/ioc/operatorModule.ts
+++ src/4_framework/ioc/operatorModule.ts
@@ -1,6 +1,66 @@
 import { ContainerModule, interfaces } from "inversify";
-import { RegisterControllerplayer } from "../../3_controller/controller/user/RegisterControllerPlayer";
+import {
+  DeleteControllerAccessProfile,
+  FindAllControllerAccessProfile,
+  FindOneControllerAccessProfile,
+  RegisterControllerAccessProfile,
+  UpdateControllerAccessProfile,
+} from "../../3_controller/controller/access";
+import {
+  DeleteControllerBet,
+  FindAllControllerBet,
+  FindOneControllerBet,
+  RegisterControllerBet,
+  UpdateControllerBet,
+} from "../../3_controller/controller/bet";
+import {
+  DeleteControllerCar,
+  FindAllControllerCar,
+  FindOneControllerCar,
+  RegisterControllerCar,
+  UpdateControllerCar,
+} from "../../3_controller/controller/car";
+import {
+  DeleteControllerGame,
+  FindAllControllerGame,
+  FindOneControllerGame,
+  RegisterControllerGame,
+  UpdateControllerGame,
+} from "../../3_controller/controller/game";
+import {
+  DeleteControllerUser,
+  FindAllControllerUser,
+  FindOneControllerUser,
+  RegisterControllerAdmin,
+  RegisterControllerPlayer,
+  UpdateControllerUser,
+} from "../../3_controller/controller/user";
 
 export const operatorModule = new ContainerModule((bind: interfaces.Bind) => {
-  bind(RegisterControllerplayer).to(RegisterControllerplayer);
+  bind(RegisterControllerPlayer).to(RegisterControllerPlayer);
+  bind(RegisterControllerAdmin).to(RegisterControllerAdmin);
+  bind(FindAllControllerUser).to(FindAllControllerUser);
+  bind(FindOneControllerUser).to(FindOneControllerUser);
+  bind(UpdateControllerUser).to(UpdateControllerUser);
+  bind(DeleteControllerUser).to(DeleteControllerUser);
+  bind(DeleteControllerGame).to(DeleteControllerGame);
+  bind(FindAllControllerGame).to(FindAllControllerGame);
+  bind(FindOneControllerGame).to(FindOneControllerGame);
+  bind(RegisterControllerGame).to(RegisterControllerGame);
+  bind(UpdateControllerGame).to(UpdateControllerGame);
+  bind(DeleteControllerCar).to(DeleteControllerCar);
+  bind(FindAllControllerCar).to(FindAllControllerCar);
+  bind(FindOneControllerCar).to(FindOneControllerCar);
+  bind(RegisterControllerCar).to(RegisterControllerCar);
+  bind(UpdateControllerCar).to(UpdateControllerCar);
+  bind(DeleteControllerBet).to(DeleteControllerBet);
+  bind(FindAllControllerBet).to(FindAllControllerBet);
+  bind(FindOneControllerBet).to(FindOneControllerBet);
+  bind(RegisterControllerBet).to(RegisterControllerBet);
+  bind(UpdateControllerBet).to(UpdateControllerBet);
+  bind(DeleteControllerAccessProfile).to(DeleteControllerAccessProfile);
+  bind(FindAllControllerAccessProfile).to(FindAllControllerAccessProfile);
+  bind(FindOneControllerAccessProfile).to(FindOneControllerAccessProfile);
+  bind(RegisterControllerAccessProfile).to(RegisterControllerAccessProfile);
+  bind(UpdateControllerAccessProfile).to(UpdateControllerAccessProfile);
 });
--- src/4_framework/ioc/useCaseModule.ts
+++ src/4_framework/ioc/useCaseModule.ts
@@ -1,10 +1,74 @@
 import { ContainerModule, interfaces } from "inversify";
-import { FindOneAccessProfileUseCase } from "../../2_business/use_cases/access_profile/findOneAccessProfileUseCase";
-import { RegisterUserUseCase } from "../../2_business/use_cases/user/registerUseCase";
-import { UpdateUserUseCase } from "../../2_business/use_cases/user/updateUseCase";
+import {
+  DeleteAccessProfileUseCase,
+  FindAllAccessProfileUseCase,
+  FindOneAccessProfileUseCase,
+  RegisterAccessProfileUseCase,
+  UpdateAccessProfileUseCase,
+} from "../../2_business/use_cases/access_profile";
+import { LoginUseCase, VerifyUseCase } from "../../2_business/use_cases/auth";
+import {
+  DeleteBetUseCase,
+  FindAllBetUseCase,
+  FindOneBetUseCase,
+  RegisterBetUseCase,
+  UpdateBetUseCase,
+} from "../../2_business/use_cases/bet";
+import {
+  DeleteCarUseCase,
+  FindAllCarUseCase,
+  FindOneCarUseCase,
+  RegisterCarUseCase,
+  UpdateCarUseCase,
+} from "../../2_business/use_cases/car";
+import {
+  DeleteGameUseCase,
+  FindAllGameUseCase,
+  FindOneGameUseCase,
+  RegisterGameUseCase,
+  UpdateGameUseCase,
+} from "../../2_business/use_cases/game";
+import {
+  DeleteUserUseCase,
+  FindAllUserUseCase,
+  FindOneUserUseCase,
+  RegisterUserUseCase,
+  UpdateUserUseCase,
+  UserLevelAccessAuthorized,
+} from "../../2_business/use_cases/user";
 
 export const useCaseModule = new ContainerModule((bind: interfaces.Bind) => {
+  bind(DeleteAccessProfileUseCase).to(DeleteAccessProfileUseCase);
+  bind(FindAllAccessProfileUseCase).to(FindAllAccessProfileUseCase);
+  bind(FindOneAccessProfileUseCase).to(FindOneAccessProfileUseCase);
+  bind(RegisterAccessProfileUseCase).to(RegisterAccessProfileUseCase);
+  bind(UpdateAccessProfileUseCase).to(UpdateAccessProfileUseCase);
+
+  bind(LoginUseCase).to(LoginUseCase);
+  bind(VerifyUseCase).to(VerifyUseCase);
+
+  bind(DeleteBetUseCase).to(DeleteBetUseCase);
+  bind(FindAllBetUseCase).to(FindAllBetUseCase);
+  bind(FindOneBetUseCase).to(FindOneBetUseCase);
+  bind(RegisterBetUseCase).to(RegisterBetUseCase);
+  bind(UpdateBetUseCase).to(UpdateBetUseCase);
+
+  bind(DeleteCarUseCase).to(DeleteCarUseCase);
+  bind(FindAllCarUseCase).to(FindAllCarUseCase);
+  bind(FindOneCarUseCase).to(FindOneCarUseCase);
+  bind(RegisterCarUseCase).to(RegisterCarUseCase);
+  bind(UpdateCarUseCase).to(UpdateCarUseCase);
+
+  bind(DeleteGameUseCase).to(DeleteGameUseCase);
+  bind(FindAllGameUseCase).to(FindAllGameUseCase);
+  bind(FindOneGameUseCase).to(FindOneGameUseCase);
+  bind(RegisterGameUseCase).to(RegisterGameUseCase);
+  bind(UpdateGameUseCase).to(UpdateGameUseCase);
+
+  bind(DeleteUserUseCase).to(DeleteUserUseCase);
+  bind(FindAllUserUseCase).to(FindAllUserUseCase);
+  bind(FindOneUserUseCase).to(FindOneUserUseCase);
   bind(RegisterUserUseCase).to(RegisterUserUseCase);
   bind(UpdateUserUseCase).to(UpdateUserUseCase);
-  bind(FindOneAccessProfileUseCase).to(FindOneAccessProfileUseCase);
+  bind(UserLevelAccessAuthorized).to(UserLevelAccessAuthorized);
 });
