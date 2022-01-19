1b0fbe1150fdac2d58d69a3a39f58ea05e063279
alanbruno1994 <bruno47rios@outlook.com>
2022-01-19 16:26:17 -0300

    refactor: removed elements not used in migration

--- src/4_framework/database/migrations/20220114130932-addCarIdinUser.js
+++ src/4_framework/database/migrations/20220114130932-addCarIdinUser.js
@@ -14,30 +14,9 @@ module.exports = {
       onDelete: "CASCADE",
       onUpdate: "CASCADE",
     });
-    await queryInterface.bulkInsert("cars", [
-      {
-        secure_id: "fa_01",
-        model: "flex",
-        color: "red",
-        year: 2000,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
-    const values = await queryInterface.sequelize.query("Select * From cars");
-    console.log(values[0]);
-    await queryInterface.changeColumn("users", "car_id", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      unique: true,
-      references: {
-        model: "cars",
-        key: "id",
-      },
-    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("users", "user_id");
+    await queryInterface.removeColumn("users", "car_id");
   },
 };
