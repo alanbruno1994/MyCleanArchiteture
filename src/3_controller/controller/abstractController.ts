8920b5df05084222893444fec7c2887483a7999d
alanbruno1994 <bruno47rios@outlook.com>
2022-01-19 16:24:23 -0300

    feat: add element for inject depency

--- src/3_controller/controller/abstractController.ts
+++ src/3_controller/controller/abstractController.ts
@@ -1,5 +1,6 @@
 import { Result } from "../../shared/Result";
 import { injectable } from "inversify";
+import "reflect-metadata";
 @injectable()
 export abstract class AbstractController {
   abstract run(...input: any[]): Promise<Result>;
