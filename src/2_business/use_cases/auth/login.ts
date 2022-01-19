bc831cc6d7280863dd401f059e03a22482503f0a
alanbruno1994 <bruno47rios@outlook.com>
2022-01-19 16:19:28 -0300

    refactor: modify use case related with login

--- src/2_business/use_cases/auth/login.ts
+++ src/2_business/use_cases/auth/login.ts
@@ -6,47 +6,37 @@ import {
   ICryptServiceSymbol,
 } from "../../services/cryptService";
 import {
-  IUserRepository,
-  IUserRepositorySymbol,
-} from "../../repositories/userRepository";
-import {
   ITokenService,
   ITokenServiceSymbol,
 } from "../../services/tokenService";
 
 type Input = {
-  email: string;
   password: string;
+  hashPassword: string;
+  id: number;
+  securedId: string;
 };
 
 @injectable()
-export class LoginUseCase extends AbstractUseCase<Input, Result> {
+export class TokenLoginUseCase extends AbstractUseCase<Input, Result> {
   constructor(
     @inject(ICryptServiceSymbol) private readonly hasherService: ICryptService,
-    @inject(IUserRepositorySymbol)
-    private readonly userRepository: IUserRepository,
     @inject(ITokenServiceSymbol)
     private readonly tokenService: ITokenService
   ) {
     super();
   }
   async run(input: Input): Promise<Result> {
-    const user = await this.userRepository.findOne({
-      where: { key: "email", valueKey: input.password },
-    });
-    if (isIError(user)) {
-      return user;
-    }
     const resultHash = await this.hasherService.compare(
       input.password,
-      user.body.password
+      input.hashPassword
     );
     if (isIError(resultHash)) {
       return resultHash;
     }
     return await this.tokenService.generate({
-      id: user.body.id,
-      securedId: user.body.securedId,
+      id: input.id,
+      securedId: input.securedId,
     });
   }
 }
